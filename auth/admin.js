8b0b93ac85af04c973e3a74b7b691cfcd84bd18f
Camilo Carromeu <camilo@carromeu.com>
2020-01-10 17:29:58 -0400

    Remoção de debug.

--- auth/admin.js
+++ auth/admin.js
@@ -1,7 +1,6 @@
 
 module.exports = function () {
   return function (req, res, next) {
-    console.log('Ok: ' + req.user.name)
     if (!req.user.admin) {
       res.status(403).json({ message: 'You\'re not admin!' })
     } else {
