f4e887712b4327e4c8962f48a0e9e47b2e5a6316
Camilo Carromeu <camilo@carromeu.com>
2020-01-30 09:48:56 -0400

    Alteração do local padrão dos endpoints genéricos.

--- business/status.js
+++ business/status.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   })
 
   app.get('/', function (req, res) {
-    var pkg = require('../../package.json')
+    var pkg = require('../package.json')
 
     res.status(200).json({ name: pkg.name, description: pkg.description, version: pkg.version })
   })
