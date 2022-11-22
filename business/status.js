172f40950cfef8d4f8e57cfcf55ff5d7e64efa10
Ygo Brito <ygo1992@gmail.com>
2022-11-21 22:39:05 -0400

    Adicionando cors

--- business/status.js
+++ business/status.js
@@ -1,10 +1,11 @@
+var cors = require('cors')
 
 module.exports = function (app) {
-  app.get('/status', function (req, res) {
+  app.get('/status', cors(), function (req, res) {
     res.status(200).json('Ok')
   })
 
-  app.get('/', function (req, res) {
+  app.get('/', cors(), function (req, res) {
     var pkg = require('../package.json')
 
     res.status(200).json({ name: pkg.name, description: pkg.description, version: pkg.version })
