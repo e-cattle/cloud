74e37064c934a56d2c8c669397176f6c708ad80c
Ygo Brito <ygo1992@gmail.com>
2023-06-19 20:42:38 -0400

    Refatoração de usuário logando pela primeira vez, removendo da lista/collection new-user para user

--- business/web/user.js
+++ business/web/user.js
@@ -4,11 +4,12 @@ module.exports = function (app) {
 
   app.get('/web/user', auth.authenticate(), function (req, res) {
     var NewUser = app.db.model('NewUser')
+    var User = app.db.model('User')
     var Farm = app.db.model('Farm')
 
     /* Verifica se o usuário logado possui vínculo pendente como usuário de alguma farm.
     Em seguida já o adiciona no array de Users da Farm com seu papel */
-    NewUser.find({ email: req.user.email }, function (error, newUsers) {
+    NewUser.find({ email: req.user.email }, async function (error, newUsers) {
       if (!error) {
         newUsers.forEach(async newUser => {
           await Farm.findOne({ _id: newUser.farm }).populate('users.user').exec(function (err, farm) {
@@ -21,19 +22,22 @@ module.exports = function (app) {
               if (indexUser !== -1) {
                 farm.users[indexUser].role = newUser.role
               } else {
-                farm.users[farm.users.length] = { user: req.user, role: newUser.role }
+                let newUserToUser = req.user
+                newUserToUser.name = newUser.name
+                farm.users[farm.users.length] = { user: newUserToUser, role: newUser.role }
               }
-              Farm.findOneAndUpdate({ _id: newUser.farm }, { users: farm.users }, function (error) {
-                if (error) res.status(501).send('Erro ao atualizar usuarios da propriedade')
+              Farm.findOneAndUpdate({ _id: newUser.farm }, { users: farm.users }, async function (error) {
+                if (error) return res.status(501).send('Erro ao atualizar usuarios da propriedade')
+                await User.findOneAndUpdate({ _id: req.user._id }, { name: newUser.name}, async function (error) {
+                  if (!error) 
+                  await NewUser.findOneAndDelete({ _id: newUser._id }, function (error) {
+                    if(!error) console.log('Usuário deletado da lista de usuários')
+                  })
+                })
               })
-              // res.status(201).json('Usuário vinculado com sucesso à propriedade ' + newUser.farm)
             }
           })
         })
-        /* await NewUser.findOneAndDelete({ _id: newUser._id }, function (error) {
-                if (!error) console.log('Usuário deletado da lista de usuários')
-              })
-              res.status(200).json('Usuário vinculado com sucesso à propriedade ' + newUser.farm) */
       }
     })
 
